a61ea5c04da33eb91a6c76ad524ba36cd05242b0
JC-Rao <mcmillan520@gmail.com>
2024-04-25 14:51:12 -0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+                hostname: '*.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+        },
+      ],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
